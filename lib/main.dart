aebca93e53f8cdc7de8b9d379e06a1ca2a7c3bb2
monir467 <monirsatt@gmail.com>
2020-11-22 21:42:20 +0600

    splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -12,6 +13,33 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    double size = MediaQuery.of(context).size.height;
+    return SafeArea(
+      child: Scaffold(
+          body: Container(
+        height: size,
+        color: Colors.white,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+                height: size * 0.7,
+                child: Align(
+                    alignment: Alignment.center,
+                    child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20.0, vertical: 20.0),
+                        child: Image.asset('assets/images/bismillahG.png')))),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 10.0),
+              child: SpinKitFoldingCube(
+                color: Colors.lightGreen,
+                size: 80.0,
+              ),
+            )
+          ],
+        ),
+      )),
+    );
   }
 }
